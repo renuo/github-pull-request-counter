e0bcd0e77126b280594934842ac3169eaabcc2d4
Janis-Leuenberger <janis.leuenberger@stud.kftg.ch>
2021-05-07 16:23:27 +0200

    Implement options UI

--- src/js/options.ts
+++ src/js/options.ts
@@ -1,12 +1,6 @@
 import '../css/options.scss';
 
 const options = () => {
-  const healthButton = document.getElementById('health-check');
-  if (healthButton) {
-    healthButton.addEventListener('click', () => {
-      alert('All good');
-    });
-  }
 };
 
 options();
