4257fd528ceb0af0138f1686a16b1724206b0829
Janis-Leuenberger <janis.leuenberger@stud.kftg.ch>
2021-05-10 17:37:25 +0200

    Extend badge setter test

--- src/js/services/badge-setter.test.ts
+++ src/js/services/badge-setter.test.ts
@@ -9,18 +9,27 @@ global.chrome = {
   }
 } as any;
 
+
 describe('BadgeSetter', () => {
   describe('update', () => {
     let record: PullRequestRecord;
+    let counter = {
+      'PullRequest-0': true,
+      'PullRequest-1': true,
+      'PullRequest-2': true,
+      'PullRequest-3': true,
+      'PullRequest-4': true
+    };
 
     beforeEach(() => {
-      BadgeSetter().update(record, {});
+      BadgeSetter().update(record, counter);
     });
 
     describe('with an empty record', () => {
       beforeAll(() => {
         record = pullRequestRecordFactory(0);
       });
+
       it('calls setBadgeText with the correct arguments', () => {
         expect(global.chrome.action.setBadgeText).toHaveBeenCalledWith({ text: '0' });
       });
@@ -34,6 +43,7 @@ describe('BadgeSetter', () => {
       beforeAll(() => {
         record = pullRequestRecordFactory(1);
       });
+
       it('calls setBadgeText with the correct arguments', () => {
         expect(global.chrome.action.setBadgeText).toHaveBeenCalledWith({ text: '2' });
       });
@@ -47,6 +57,7 @@ describe('BadgeSetter', () => {
       beforeAll(() => {
         record = pullRequestRecordFactory(5);
       });
+
       it('calls setBadgeText with the correct arguments', () => {
         expect(global.chrome.action.setBadgeText).toHaveBeenCalledWith({ text: '10' });
       });
@@ -54,6 +65,26 @@ describe('BadgeSetter', () => {
       it('calls setBadgeBackgroundColor with the correct arguments', () => {
         expect(global.chrome.action.setBadgeBackgroundColor).toHaveBeenCalledWith({ color: '#d9534f' });
       });
+
+      describe('with some of the counter set to false', () => {
+        beforeAll(() => {
+          counter = {
+            'PullRequest-0': false,
+            'PullRequest-1': true,
+            'PullRequest-2': false,
+            'PullRequest-3': true,
+            'PullRequest-4': false
+          };
+        })
+
+        it('calls setBadgeText with the correct arguments', () => {
+          expect(global.chrome.action.setBadgeText).toHaveBeenCalledWith({ text: '4' });
+        });
+
+        it('calls setBadgeBackgroundColor with the correct arguments', () => {
+          expect(global.chrome.action.setBadgeBackgroundColor).toHaveBeenCalledWith({ color: '#d9534f' });
+        });
+      })
     });
   });
 });
