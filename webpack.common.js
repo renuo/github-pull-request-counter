51b6d1cda301843a25190057d364fa8f292a8ebc
Janis-Leuenberger <janis.leuenberger@stud.kftg.ch>
2021-05-12 08:44:19 +0200

    Remove dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -2,8 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
-// TODO add to documentation
 
 module.exports = {
   entry: {
@@ -53,7 +51,6 @@ module.exports = {
         { from: './src/icons/icon128.png' },
       ],
     }),
-    new Dotenv()
   ],
   output: { filename: '[name].js', path: path.resolve(__dirname, 'dist') },
 };
