027b06ccd6159a67ed62c133ab4f3b5cfdb0b5c5
Oliver Thoma <oliver.thoma@hey.com>
2025-08-14 13:07:08 +0200

    Drop Jest coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,30 @@
 export default {
-  preset: 'jest-puppeteer',
-  transform: {
-    '^.+\\.js$': ['babel-jest', {
-      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
-      plugins: ['@babel/plugin-transform-modules-commonjs']
-    }]
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)/)'
-  ],
-  globals: {
-    ENV: 'testing',
-  },
-  "moduleNameMapper": {
-    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "src/js/**/*.js",
-    "!**/node_modules/**",
-    "!**/dist/**",
-    "!**/mocks/**",
-    "!**/coverage/**",
-    "!jest.config.js",
-  ],
-  testPathIgnorePatterns: [
-    "/test/integration/"
-  ],
-  coverageThreshold: {
-    "global": {
-      "branches": 100,
-      "functions": 100,
-      "lines": 100,
-      "statements": 100
-    }
-  },
+    preset: 'jest-puppeteer',
+    transform: {
+        '^.+\\.js$': ['babel-jest', {
+            presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+            plugins: ['@babel/plugin-transform-modules-commonjs']
+        }]
+    },
+    transformIgnorePatterns: [
+        'node_modules/(?!(node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)/)'
+    ],
+    globals: {
+        ENV: 'testing',
+    },
+    "moduleNameMapper": {
+        "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/js/**/*.js",
+        "!**/node_modules/**",
+        "!**/dist/**",
+        "!**/mocks/**",
+        "!**/coverage/**",
+        "!jest.config.js",
+    ],
+    testPathIgnorePatterns: [
+        "/test/integration/"
+    ]
 }
